276dc78b76d1e78ceaa5bd3301d4c560b3c4ed13
DProgrammingLenguaje <87881594+DProgrammingLenguaje@users.noreply.github.com>
2021-07-23 20:58:13 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,6 @@ $("body").on("contextmenu", function(e) {
 $("html").on("contextmenu", function(e) {
     return false;
 });
+
+/*Si llegaste a este código, ¡felicidades! este JavaScript se implementa para que la página no pueda ser inspeccionada. 
+Es tu misión averiguar porqué no es aplicado en esta página. De hacerlo, comunicate con administración explicando el porqué, ¡Te daremos un premio!*/
